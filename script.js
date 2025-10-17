215f1261940d0b4da540da15f8a803e4c1cebaa6
Camilia <161958081+fromwonderland@users.noreply.github.com>
2025-10-17 17:28:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,11 @@ const fileContent = document.getElementById('file-content');
 // Données des chapitres et fichiers
 const appData = {
     'Notes de cours': ['NotesCours.pdf'],
-    'Certifications': ['Certification_PIX_Camilia_SABA.pdf'],
+    'Dossier 1: INTRODUCTION AUX SYSTEMES D INFORMATION': ['DOSSIER1_td_PGI_exercice.pdf'],
+    'Dossier 2: ACTEURS ET FLUX': ['DOSSIER2_TP_Odoo.pdf'],
+    'Dossier 3: LIVRABLES': ['DOSSIER3 Training schéma réponse.pdf', 'DOSSIER3_ProcessusGestion_Activites_Acteurs.pdf', 'DOSSIER3_Les livrables_1.pdf','DOSSIER3_Les livrables_2.pdf'],
+    'Dossier 4: DIAGRAMME DE GANTT': ['DOSSIER4_Diagramme_Gantt.pdf'],
+    'Certifications': ['Camilia_SABA_Certification_PIX.pdf'],
     'Projet final': [],
     'Soutenance (diaporama)': [],
     'Autres documents': []
